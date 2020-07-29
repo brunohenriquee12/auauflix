fc3fd1c44d2a983192813edee28a1121276a60a2
brunohenriquee12 <brunohscosta@outlook.com>
2020-07-29 14:01:28 -0300

    Commit adding "cadastro video" page and more

--- auauflix/src/components/Menu/index.js
+++ auauflix/src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png'
 import './Menu.css';
 import Button from '../components/Button';
@@ -6,13 +7,13 @@ import Button from '../components/Button';
 function Menu() {
     return (
         <nav className="Menu">
-        <a href="/">
-        <img className="Logo" src={Logo} alt= "AuauFlix logo"/>
-         </a>
+        <Link to="/">
+            <img className="Logo" src={Logo} alt= "AuauFlix logo"/>
+         </Link>
 
-<Button as="a" className="ButtonLink" href="/">
-        Novo Vídeo
-</Button>
+        <Button as={ Link } className="ButtonLink" to="/cadastro/video">
+             Novo vídeo
+        </Button>
 
         </nav>
     );
